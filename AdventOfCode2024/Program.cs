f60c684a7f4b23e7a00290aa06f47a15e1ffa6f2
Carl Henning Haugen <chh@sealengineering.no>
2024-12-04 15:53:39 +0100

    Day 03 Puzzle 02 Solved!

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -28,6 +28,7 @@ internal class Program
         puzzles.AddPuzzle<Day02Puzzle01>(logger, ResourceDirectoryInfo);
         puzzles.AddPuzzle<Day02Puzzle02>(logger, ResourceDirectoryInfo);
         puzzles.AddPuzzle<Day03Puzzle01>(logger, ResourceDirectoryInfo);
+        puzzles.AddPuzzle<Day03Puzzle02>(logger, ResourceDirectoryInfo);
 
         WriteLine("Which puzzle do you want to solve?");
         int puzzleI = 0;
