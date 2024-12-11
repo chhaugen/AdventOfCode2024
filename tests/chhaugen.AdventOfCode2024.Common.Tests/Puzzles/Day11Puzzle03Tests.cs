e02ff273703cb80208d2453986becfc2b6447d0d
Carl Henning Haugen <chh@sealengineering.no>
2024-12-11 14:24:51 +0100

    XUnit Output

--- tests/chhaugen.AdventOfCode2024.Common.Tests/Puzzles/Day11Puzzle03Tests.cs
+++ tests/chhaugen.AdventOfCode2024.Common.Tests/Puzzles/Day11Puzzle03Tests.cs
@@ -1,11 +1,19 @@
 ﻿using chhaugen.AdventOfCode2024.Common.Puzzles;
 using chhaugen.AdventOfCode2024.Resources;
 using System.Numerics;
+using Xunit.Abstractions;
 
 namespace chhaugen.AdventOfCode2024.Common.Tests.Puzzles;
 
 public class Day11Puzzle03Tests
 {
+    private readonly ITestOutputHelper _output;
+
+    public Day11Puzzle03Tests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
     private readonly PuzzleInputs _inputs = new();
 
     [Theory]
@@ -17,7 +25,7 @@ public class Day11Puzzle03Tests
     public async Task SolveAsync_UsingExampleInput(int times, string count)
     {
         // Arrange
-        Day11Puzzle03 puzzle = new();
+        Day11Puzzle03 puzzle = new(progressOutput: _output.WriteLine);
         string input = await _inputs.GetInputAsync(day: 11, "example.txt");
         // Act
         string output = await puzzle.SolveAsync(input, times);
@@ -32,10 +40,11 @@ public class Day11Puzzle03Tests
     [InlineData(200, "13010119344002949133898595086994668979")]
     [InlineData(500, "37606667031737277464887525086188379202237469282192933802518986560424519998378572700789498897")]
     //[InlineData(1000, "220582363471151968280170102882767382933850413179724668923921439702820670848595211677467884355272289538638202439741039235942169752525022851406779817242302208765379391808020477618944857")]
+    //[InlineData(1500, "1293828539324072587132845789352368027930895902387733271705656042406030799692638202725272947783254098866935890876356703466726905622282438001087043576949777045952305764305911754747031847456447724142102430401030939633884144940560525909860009002932884791356894873335969773140514")]
     public async Task SolveAsync_UsingRealInput(int times, string count)
     {
         // Arrange
-        Day11Puzzle03 puzzle = new();
+        Day11Puzzle03 puzzle = new(progressOutput: _output.WriteLine);
         string input = await _inputs.GetInputAsync(day: 11, "input.txt");
         // Act
         string output = await puzzle.SolveAsync(input, times);
