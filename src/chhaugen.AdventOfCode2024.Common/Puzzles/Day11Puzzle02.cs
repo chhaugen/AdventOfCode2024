67edcabf9851f09acb00bfa564c60003b1d1a634
Carl Henning Haugen <chh@sealengineering.no>
2024-12-11 12:45:42 +0100

    Added some reporting to Day 11

--- src/chhaugen.AdventOfCode2024.Common/Puzzles/Day11Puzzle02.cs
+++ src/chhaugen.AdventOfCode2024.Common/Puzzles/Day11Puzzle02.cs
@@ -1,4 +1,5 @@
-﻿using static chhaugen.AdventOfCode2024.Common.Puzzles.Day11Puzzle01;
+﻿using System.Text.Json;
+using static chhaugen.AdventOfCode2024.Common.Puzzles.Day11Puzzle01;
 
 namespace chhaugen.AdventOfCode2024.Common.Puzzles;
 public class Day11Puzzle02 : Puzzle
@@ -13,12 +14,37 @@ public class Day11Puzzle02 : Puzzle
         var initialStones = ParseInput(input);
 
         var times = 75;
-        Dictionary<(int times, long stone), long> stoneCache = [];
-        var counts = initialStones.Select(x => GetBlinkStonesCount(x, times, stoneCache)).ToList();
-        return Task.FromResult(counts.Select(x => (long)x).Sum().ToString());
+        Dictionary<StoneTimes, long> stoneCache = [];
+        Dictionary<StoneTimes, int>? cacheHitsCount = null;
+#if DEBUG
+        cacheHitsCount = [];
+#endif
+
+        var counts = initialStones.Select(x => GetBlinkStonesCount(x, times, stoneCache, cacheHitsCount)).ToList();
+
+        var count = counts.Sum().ToString();
+
+        _progressOutput($"CacheSize: {stoneCache.Count:D4}, Count: {count}");
+
+#if DEBUG
+        SaveCacheResults(stoneCache, cacheHitsCount);
+#endif
+        return Task.FromResult(count.ToString());
     }
 
-    public long GetBlinkStonesCount(long stone, int times, Dictionary<(int times, long stone), long> cache)
+    internal void SaveCacheResults(Dictionary<StoneTimes, long> stoneCache, Dictionary<StoneTimes, int> cacheHitsCount)
+    {
+        using FileStream outFile = File.Create($"{nameof(Day11Puzzle02)}Cache.json");
+        var jsonList = stoneCache
+            .Join(cacheHitsCount, x => x.Key, x => x.Key, (x, y) => new { x.Key.Times, x.Key.Stone, Count = x.Value, Hit = y.Value })
+            .OrderByDescending(x => x.Count)
+            .ThenByDescending(x => x.Hit)
+            .ThenByDescending(x => x.Times)
+            .ToList();
+        JsonSerializer.Serialize(outFile, jsonList, options: new() { WriteIndented = true });
+    }
+
+    public long GetBlinkStonesCount(long stone, int times, Dictionary<StoneTimes, long> cache, Dictionary<StoneTimes, int>? cacheHitsResults = null)
     {
         if (times == 0)
             return 1;
@@ -27,7 +53,7 @@ public class Day11Puzzle02 : Puzzle
         if (stone == 0)
         {
             long newStoneOne = 1;
-            count = GetBlinkStonesCountCached(newStoneOne, times - 1, cache);
+            count = GetBlinkStonesCountCached(newStoneOne, times - 1, cache, cacheHitsResults);
 
         }
 
@@ -38,35 +64,51 @@ public class Day11Puzzle02 : Puzzle
             {
                 var halfLength = stoneLength / 2;
                 var firstStone = stone / (long)Math.Pow(10, halfLength);
-                count += GetBlinkStonesCountCached(firstStone, times - 1, cache);
+                count += GetBlinkStonesCountCached(firstStone, times - 1, cache, cacheHitsResults);
 
                 var secondStone = stone % (long)Math.Pow(10, halfLength);
-                count += GetBlinkStonesCountCached(secondStone, times - 1, cache);
+                count += GetBlinkStonesCountCached(secondStone, times - 1, cache, cacheHitsResults);
             }
         }
 
         if (count == 0)
         {
             long newStone = stone * 2024;
-            count = GetBlinkStonesCountCached(newStone, times - 1, cache);
+            count = GetBlinkStonesCountCached(newStone, times - 1, cache, cacheHitsResults);
         }
 
         if (times > 25)
         {
-            _progressOutput($"[{times:D2}] CacheSize: {cache.Count:D4} , Count: {count}");
+            _progressOutput($"[{times:D2}] CacheSize: {cache.Count:D4}, Count: {count}");
         }
         return count;
     }
 
-    internal long GetBlinkStonesCountCached(long stone, int times, Dictionary<(int times, long stone), long> cache)
+    internal long GetBlinkStonesCountCached(long stone, int times, Dictionary<StoneTimes, long> cache, Dictionary<StoneTimes, int>? cacheHitsResults = null)
     {
-        if (cache.TryGetValue((times, stone), out long stoneCachedCount))
+        StoneTimes stoneTimes = new(stone, times);
+        if (cache.TryGetValue(stoneTimes, out long stoneCachedCount))
+        {
+            RecordCacheHit(stoneTimes, cacheHitsResults);
+
             return stoneCachedCount;
+        }
         else
         {
-            long calculatedStoneValue = GetBlinkStonesCount(stone, times, cache);
-            cache[(times, stone)] = calculatedStoneValue;
+            long calculatedStoneValue = GetBlinkStonesCount(stone, times, cache, cacheHitsResults);
+            cache[stoneTimes] = calculatedStoneValue;
             return calculatedStoneValue;
+        }
+    }
+
+    private static void RecordCacheHit(StoneTimes stoneTimes, Dictionary<StoneTimes, int>? cacheHitsResults)
+    {
+        if (cacheHitsResults is not null)
+        {
+            if (cacheHitsResults.TryGetValue(stoneTimes, out int lookUpCount))
+                cacheHitsResults[stoneTimes] = ++lookUpCount;
+            else
+                cacheHitsResults[stoneTimes] = 1;
         }
     }
 
@@ -75,5 +117,16 @@ public class Day11Puzzle02 : Puzzle
         if (num == 0)
             return 1;
         return (int)Math.Log10(num) + 1;
+    }
+
+    public readonly struct StoneTimes
+    {
+        public StoneTimes(long stone, int times)
+        {
+            Stone = stone;
+            Times = times;
+        }
+        public long Stone { get; }
+        public int Times { get; }
     }
 }
