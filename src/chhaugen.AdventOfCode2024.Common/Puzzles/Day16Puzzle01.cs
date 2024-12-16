811eea17005bea29c4aaa8b4fa4335bf3124971f
Carl Henning Haugen <chh@sealengineering.no>
2024-12-17 00:20:39 +0100

    Day 15 Puzzle 01 Works on examples

---
+++ src/chhaugen.AdventOfCode2024.Common/Puzzles/Day16Puzzle01.cs
@@ -0,0 +1,153 @@
+﻿using chhaugen.AdventOfCode2024.Common.Extentions;
+using chhaugen.AdventOfCode2024.Common.Structures;
+using System.Threading.Tasks.Dataflow;
+
+namespace chhaugen.AdventOfCode2024.Common.Puzzles;
+public class Day16Puzzle01 : Puzzle
+{
+    public Day16Puzzle01(Action<string>? progressOutput = null) : base(progressOutput)
+    {
+    }
+
+    public override Task<string> SolveAsync(string input)
+    {
+        Map2D<char> map = Map2D.ParseInput(input);
+
+        Point2D start = map
+            .AsPointEnumerable()
+            .First(x => map[x] == 'S');
+
+        Point2D end = map
+            .AsPointEnumerable()
+            .First(x => map[x] == 'E');
+
+        Reindeer reindeer = new(start, CardinalDirection.East);
+
+        var graf = CreateReindeerPathGraf(reindeer, map);
+
+        var endLeafs = graf
+            .GetLeafs()
+            .Where(x => map[x.Value.Position] == 'E')
+            .ToList();
+
+        var endLeafCount = endLeafs.Count();
+
+        var paths = endLeafs
+            .Select(x => x.GetAncestors().Select(x => x.Value).ToList())
+            .ToList();
+
+        paths.ForEach(x => x.Reverse());
+
+        var scores = paths
+            .Select(GetScore)
+            .ToList();
+        var minScore = scores.Min();
+
+        return Task.FromResult(minScore.ToString());
+    }
+
+    public static int GetScore(IEnumerable<Reindeer> reindeers)
+    {
+        int score = 0;
+        Reindeer? previous = null;
+        foreach (var reindeer in reindeers)
+        {
+            if (previous == null)
+            {
+                previous = reindeer;
+                continue;
+            }
+
+            // Turn
+            if (previous.Value.Direction != reindeer.Direction)
+                score += 1000;
+
+            // Walk forward
+            score++;
+            
+            previous = reindeer;
+        }
+        return score;
+    }
+
+    public static Node<Reindeer> CreateReindeerPathGraf(Reindeer start, Map2D<char> map)
+    {
+        List<Point2D> breadcrumbs = [];
+        List<Point2D> deadEnds = [];
+        Node<Reindeer> root = ScanPathsRecursivly(start, map, breadcrumbs, deadEnds);
+        return root;
+    }
+
+    public static Node<Reindeer> ScanPathsRecursivly(Reindeer reindeer, Map2D<char> map, List<Point2D> breadcrumbs, List<Point2D> deadEnds)
+    {
+        Node<Reindeer> parent = new(reindeer);
+        var reindeersToCheck = GetReindeersToCheck(reindeer);
+        foreach (var reindeerToCheck in reindeersToCheck)
+        {
+            if (map[reindeerToCheck.Position] == '.' && !breadcrumbs.Contains(reindeerToCheck.Position))
+            {
+                breadcrumbs.Add(reindeerToCheck.Position);
+                var newChild = ScanPathsRecursivly(reindeerToCheck, map, [.. breadcrumbs], deadEnds);
+                newChild.Parent = parent;
+                parent.Children.Add(newChild);
+            }
+            else if (map[reindeerToCheck.Position] == 'E')
+                parent.Children.Add(new(reindeerToCheck, parent));
+        }
+        return parent;
+    }
+
+    public static IEnumerable<Reindeer> GetReindeersToCheck(Reindeer reindeer)
+    {
+        CardinalDirection right = reindeer.Direction.TurnClockwise();
+        Point2D pointRight = reindeer.Position.GetPointInDirection(right);
+        yield return new(pointRight, right);
+
+        Point2D pointInFront = reindeer.Position.GetPointInDirection(reindeer.Direction);
+        yield return new(pointInFront, reindeer.Direction);
+
+        CardinalDirection left = reindeer.Direction.TurnAntiClockwise();
+        Point2D pointLeft = reindeer.Position.GetPointInDirection(left);
+        yield return new(pointLeft, left);
+    }
+
+
+    public readonly struct Reindeer : IEquatable<Reindeer> 
+    {
+        public Reindeer(Point2D position, CardinalDirection direction)
+        {
+            Position = position;
+            Direction = direction;
+        }
+
+        public Point2D Position { get; }
+        public CardinalDirection Direction { get; }
+
+        public Reindeer TurnClockwise()
+            => new(Position, Direction.TurnClockwise());
+        public Reindeer TurnAntiClockwise()
+            => new(Position, Direction.TurnAntiClockwise());
+
+        public override string ToString()
+            => $"{Position} {Enum.GetName(Direction)}";
+
+        public override int GetHashCode()
+            => HashCode.Combine(Position, Direction);
+
+        public bool Equals(Reindeer other)
+            => Position == other.Position && Direction == other.Direction;
+
+        public override bool Equals(object? obj)
+            => obj is Reindeer other && Equals(other);
+
+        public static bool operator ==(Reindeer left, Reindeer right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Reindeer left, Reindeer right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ src/chhaugen.AdventOfCode2024.Common/Structures/Node[T].cs
@@ -0,0 +1,59 @@
+﻿namespace chhaugen.AdventOfCode2024.Common.Structures;
+public class Node<T> : ICloneable
+{
+    public Node(T value)
+    {
+        Value = value;
+    }
+
+    public Node(T value, Node<T>? parent)
+        : this(value)
+    {
+        Parent = parent;
+    }
+
+    public Node(T value, Node<T>? parent, params List<Node<T>> children)
+        : this(value, parent)
+    {
+        Children = children;
+    }
+
+    public T Value { get; set; }
+
+    public Node<T>? Parent { get; set; }
+
+    public List<Node<T>> Children { get; set; } = [];
+
+    public bool IsLeaf => Children.Count == 0;
+
+    public IEnumerable<Node<T>> GetAncestors()
+    {
+        yield return this;
+        if (Parent != null)
+            foreach(var node in Parent.GetAncestors())
+                yield return node;
+    }
+
+    public IEnumerable<Node<T>> GetLeafs()
+    {
+        if (IsLeaf)
+        {
+            yield return this;
+        }
+        else
+        {
+            foreach (var child in Children)
+            {
+                var leafs = child.GetLeafs();
+                foreach (var leaf in leafs)
+                    yield return leaf;
+            }
+        }
+    }
+
+    public Node<T> Clone()
+        => new(Value, Parent, [.. Children]);
+
+    object ICloneable.Clone()
+        => Clone();
+}
